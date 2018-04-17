2d6531cbfd95bbd803b71cc9d96fc93c07804c9f
Malek salem <malek.salem.14@gmail.com>
2018-04-17 10:02:01 +0100

    Initial commit

---
+++ run.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+# https://flask-socketio.readthedocs.io/en/latest/
+# https://github.com/socketio/socket.io-client
+
+app = Flask(__name__)
+
+app.config[ 'SECRET_KEY' ] = 'jsbcfsbfjefebw237u3gdbdc'
+socketio = SocketIO( app )
+@app.route('/')
+def index():
+	return render_template("app.html")
+
+def messageRecived():
+  print( 'message was received!!!' )
+
+@socketio.on( 'my event' )
+def handle_my_custom_event( json ):
+  print( 'recived my event: ' + str( json ) )
+  socketio.emit( 'my response', json, callback=messageRecived )
+
+
+
+if __name__ == '__main__':
+  socketio.run( app, debug = True )
